c832cac43df675b0272e9bf8c5ad885cae6bc3e3
nivedhanr <nivedharenahan@gmail.com>
2022-11-21 10:23:59 +0530

    second commit

--- src/addition.java
+++ src/addition.java
@@ -1,5 +1,12 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class addition {
     public static void main(String[] args) {
-
+        int a;
+        Scanner s=new Scanner(System.in);
+        System.out.println("1.Addition\n2.Subtraction\n3.Multiplication\n4.Division");
+        System.out.println("Enter your choice:");
+        a=s.nextInt();
     }
 }
